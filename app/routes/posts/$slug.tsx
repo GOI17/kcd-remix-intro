12897780d4f40f482182e9f468aad20e040d48cd
Gilberto Olivas <jose.olivas@bigcommerce.com>
2022-05-26 20:33:44 -0700

    display the markdown of the post

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -2,29 +2,32 @@ import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
+import { marked } from "marked";
 import invariant from "tiny-invariant";
 
 import type { Post } from "~/models/post.server";
 import { getPost } from "~/models/post.server";
 
-type LoaderData = { post: Post };
+type LoaderData = { post: Post; html: string };
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.slug, `params.slug is required`);
   const post = await getPost(params.slug);
   invariant(post, `post ${params.slug} not found`);
+  const html = marked(post.markdown);
 
-  return json<LoaderData>({ post });
+  return json<LoaderData>({ post, html });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData();
+  const { post, html } = useLoaderData();
 
   return (
     <main className="max-w-4-xl mx-auto">
       <h1 className="my-6 border-b-2 text-center text-3xl">
         Some post: {post.title}
       </h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
