82e05e5b5c3ac41de0faf19613105f1143904eb5
Gilberto Olivas <jose.olivas@bigcommerce.com>
2022-05-26 19:22:53 -0700

    fetching data from server using loader function

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,43 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+type Post = {
+  slug: string;
+  title: string;
+};
+
+type LoaderData = {
+  posts: Post[];
+};
+
+export const loader = async () => {
+  const posts = [
+    { slug: "my-first-post", title: "My First Post" },
+    { slug: "90s-mixtape", title: "A Mixtape I Made Just For You" },
+  ];
+
+  return json<LoaderData>({ posts });
+};
+
 export default function Posts() {
+  const { posts } = useLoaderData<LoaderData>();
+
+  const postList = (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.slug}>
+          <Link to={post.slug} className="text-blue-600 underline">
+            {post.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+
   return (
     <main>
-      <h3>Hello from Posts page!</h3>
+      <h1>Posts</h1>
+      {postList}
     </main>
   );
 }
